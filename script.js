c70fc243b60e0e09ddac7a0b612bd2ff11db05fd
EMON AHMED RANA <emon9996a@gmail.com>
2025-11-13 14:09:13 +0600

    skill section add

--- script.js
+++ script.js
@@ -1,17 +1,5 @@
-document.addEventListener('DOMContentLoaded', () => {
-const skills = document.querySelectorAll('[data-skill]');
-const observer = new IntersectionObserver(entries => {
-entries.forEach(entry => {
-if (entry.isIntersecting) {
-const skill = entry.target;
-skill.classList.remove('opacity-0','translate-y-3');
-skill.classList.add('opacity-100','translate-y-0');
-const bar = skill.querySelector('.bg-pink-400');
-const width = skill.getAttribute('data-width');
-bar.classList.add(width);
-observer.unobserve(skill);
-}
-});
-}, { threshold: 0.3 });
-skills.forEach(s => observer.observe(s));
-});
+ window.addEventListener('load', () => {
+      document.querySelectorAll('.progress-bar').forEach(bar => {
+        bar.style.width = bar.style.getPropertyValue('--progress');
+      });
+    });
